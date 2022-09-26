b5bd509b0e7b0ce56a6fbd7ed2c0395f0aa16cb1
mun8123 <mun8123@gmail.com>
2022-09-26 15:25:19 +0900

    feat: 로그인 기능 추가 - 인증 토큰 발급 후 프로필 데이터 요청 - 프로필 페이지로 이동

--- src/page/login.ts
+++ src/page/login.ts
@@ -1,4 +1,5 @@
 /* eslint-disable no-restricted-globals */
+import axios from 'axios';
 import template from 'page/login.template';
 
 class Login {
@@ -14,9 +15,39 @@ class Login {
     this.template = template;
   }
 
+  private onSubmit = e => {
+    e.preventDefault();
+
+    const loginData = {
+      username: 'kminchelle',
+      password: '0lelplR',
+    };
+
+    axios
+      .post('http://localhost:8080/auth/login', loginData)
+      .then(res => res.data.result)
+      .then(({ id, token }) => {
+        axios
+          .get(`http://localhost:8080/user/${id}`, {
+            headers: {
+              Authorization: token,
+            },
+          })
+          .then(res => res.data.result)
+          .then(profile => {
+            location.href = '#profile';
+          });
+      });
+  };
+
+  private addEvent = () => {
+    this.container.addEventListener('submit', this.onSubmit);
+  };
+
   render = () => {
     this.title.innerText = '로그인';
     this.container.innerHTML = this.template({ loginFail: true });
+    this.addEvent();
   };
 }
 
